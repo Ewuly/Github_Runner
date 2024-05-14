ab8051f1db6ad82156b4dbc1e5a1f714146d2c72
ewanham <72886617+Ewuly@users.noreply.github.com>
2024-05-14 14:33:37 +0200

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,2 +1,6 @@
 RUNNER_ALLOW_RUNASROOT="1" ./config.sh --url https://github.com/Ewuly/Run_by_myself --token ARMCSWLNEXGZEZEMYDVB673GING7I
 RUNNER_ALLOW_RUNASROOT="1" ./run.sh
+
+# Start a simple HTTP server to keep the container running and listening on port 8080
+echo "Starting a simple HTTP server on port 8080 to keep the container alive"
+python3 -m http.server 8080
